75fb1f8bbf1a5da60576b4d7dd6d2342dce971bc
Ben Thomasson <bthomass@redhat.com>
2022-04-27 10:30:59 -0400

    cds to script location

--- run_app.sh
+++ run_app.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -ex
+HERE=$(dirname "$0")
+cd ${HERE}
 (nohup env $(cat .env | xargs) poetry run uvicorn --host=0.0.0.0 app.main:app 1>/dev/null 2>&1) &
 mypid=$!
 echo "pid: $mypid" > fact.yml
